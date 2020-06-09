4cb5df9a3fe4fa71ec44dfc0fab82c6c8b406dc7
Quivun <Quivun@users.noreply.github.com>
2020-06-09 18:42:15 -0500

    Update main.cpp
    Might just break me

--- PA2/main.cpp
+++ PA2/main.cpp
@@ -7,106 +7,99 @@
 #include <fcntl.h>
 #include <cstring>
 #include <string.h>
-
+#include <vector>
 using namespace std;
 
-void shell()
+vector<string> txtSplit(string inp, string divide)
 {
-    /*
-    bool quit = false;
-    while (!quit)
+    vector<string> ret;
+    string cur = "";
+    for (int q = 0; q < inp; q++)
     {
-        cout << "Shell$ ";
-        string inputline;
-        getline(cin, inputline); // get a line from standard input
-        if (inputline == string("exit"))
+        if (divide == inp.substr(q, 1))
         {
-            cout << "Shell Exiting" << endl;
-            quit = true;
+            ret.push_back(cur);
+            cur = "";
         }
         else
         {
-            int pid = fork();
-            if (pid == 0)
-            {   
-                //child process
-                // preparing the input command for execution
-                char *args[] = {(char *)inputline.c_str(), NULL};
-                execvp(args[0], args);
-            }
-            else
-            {
-                waitpid(pid, 0, 0); // wait for the child process
-                // we will discuss why waitpid() is preferred over wait()
-            }
+            cur += inp.substr(q, 1);
         }
     }
-    */
+    if (cur.length() != 0)
+    {
+        ret.push_back(cur);
+    }
+    return ret;
+}
 
-    /*
-    char buf[10];
-    int fds[2];
-    pipe(fds);
-    if (!fork())
-    {
-        // on the child side
-        char *msg = "a test message";
-        printf("CHILD: Sent %s\n", msg);
-        write(fds[1], msg, strlen(msg) + 1);
-    }
-    else
-    {
-        char buf[100];
-        read(fds[0], buf, 100);
-        printf("PARENT:Recv %s\n", buf);
-    }
-    */
+void shell()
+{
     while (true)
     {
-        // continue until the user enters a blank like
-        string line = read a line from the user;
-        // now split the line by | symbol that gives us separate commands
-        // You NEED to write this function
-        // for instance: ls -la | grep Jul | grep . | grep .cpp
-        vector<string> c = split(line, "|");
-        //after the above, levels.size() == 4, because there are
-        // 3 pipe symbols making 4 pipe levels
-        for (int i = 0; i < c.size(); i++)
+        cout << "ShellCMDLine$ ";
+        string inputline;
+        if (inputline == string("exit"))
         {
-            // set up the pipe
-            int fd[2];
-            pipe(fd);
+            cout << "Shell Exiting" << endl;
+            break;
+        }
+        else
+        {
+            getline(cin, inputline); // get a line from standard input
+            vector<string> cmdList = txtSplit(inputline, "|");
             int pid = fork();
-            if (!pid)
+            if (cmdList.size() > 1)
             {
-                // in the child process
-                // 1. redirect the output to the next level
-                // 2. execute the command at this level
-                if (i < levels.size() - 1)
+                // Regular input output
+                int pid = fork();
+                if (pid == 0)
                 {
-                    dup2(fd[1], 1);
-                    // redirect STDOUT to fd[1], so that it can write to the other side be closed
+                    char *args[] = {(char *)inputline.c_str(), NULL};
+                    execvp(args[0], args);
+                }
+                else
+                {
+                    waitpid(pid, 0, 0); // wait for the child process
+                    // we will discuss why waitpid() is preferred over wait()
                 }
             }
             else
             {
-                // in the parent process
-                if (i == levels.size() - 1)
-                { // wait only for the last child
-                    waitpid(cid);
+                // Has piping and must now do the deed
+                for (int q = 0; q < cmdList.size(); q++)
+                {
+                    int fd[2];
+                    pipe(fd);
+                    int pid = fork();
+                    if (!pid) // Child
+                    {
+                        if (q < cmdList.size() - 1)
+                        {
+                            dup2(fd[1], 1); // redirect STDOUT to fd[1], so that it can write to the other side be closed
+                        }
+                        char *args[] = {(char *)cmdList[q].c_str(), NULL};
+                        execvp(args[0], args);
+                    
+                    }
+                    else
+                    { // Parent
+                        if (q == cmdList.size() - 1)
+                        {
+                            waitpid(pid);
+                        }
+                        dup2(fd[0], 0); // now redirect the input for the next loop iteration
+                        close(fd[1]);
+                        /*1. wait for the child process running the current level command */
+                        //2. redirect input from the child process
+                    }
                 }
-                dup2(fd[0], 0);
-                // now redirect the input for the next loop iteration
-                close(fd[1]);
-                /*1. wait for the child process running the current level command */
-                //2. redirect input from the child process
             }
         }
     }
-}
 
-int main()
-{
-    shell();
-    return 0;
-}
+    int main()
+    {
+        shell();
+        return 0;
+    }
